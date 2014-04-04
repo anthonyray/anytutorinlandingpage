1c0d47d5c8b71dc95f5e78429c83375238724aef
Anthony Reinette <anthonyreinette@MacBook-Air-de-Anthony.local>
2014-04-04 13:02:47 +0200

    design v0

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('Bienvenue sur Anytutorin ! Tu es développeur et veux travailler avec nous ? Contactes nous sur : jobs@anytutor.in !');
+
+/*
+* Vertical scrolling effect
+*/
+$(function(){
+	'use strict';
+	$('.lead').bind('click',function(event){
+        var $anchor = $(this);
+		$('html, body').stop().animate({ scrollTop: $($anchor.attr('href')).offset().top }, 700);
+		event.preventDefault();
+	});
+  
+});
